6b2dd1532a2fe75dbd463b516292cc3215468e49
Andrey <42178892+arxipovdev@users.noreply.github.com>
2025-03-18 21:35:33 +0300

    Update arch_macbook_install.sh

--- arch_macbook_install.sh
+++ arch_macbook_install.sh
@@ -6,8 +6,8 @@
 
 # Проверка аргументов
 if [ $# -ne 4 ]; then
-  echo "Использование: $0 <корневой_раздел> <efi_раздел> <часовой_пояс> <пароль>"
-  echo "Пример: $0 /dev/nvme0n1p3 /dev/nvme0n1p1 Europe/Moscow \"MyPass123\""
+  echo "Usage: $0 <root_partition> <efi_partition> <timezone> <password>"
+  echo "Example: $0 /dev/nvme0n1p3 /dev/nvme0n1p1 Europe/Moscow \"MyPass123\""
   exit 1
 fi
 
@@ -17,33 +17,40 @@ EFI_PART="$2"
 TIMEZONE="$3"
 PASSWORD="$4"
 
-# Монтирование
+# Монтирование разделов
+echo "Mounting partitions..."
 umount -R /mnt 2>/dev/null
-mount $ROOT_PART /mnt
+mount "$ROOT_PART" /mnt
 mkdir -p /mnt/boot
-mount $EFI_PART /mnt/boot
+mount "$EFI_PART" /mnt/boot
+
+# Инициализация ключей
+echo "Initializing package keys..."
+arch-chroot /mnt pacman-key --init
+arch-chroot /mnt pacman-key --populate archlinux
+arch-chroot /mnt pacman -Sy archlinux-keyring --noconfirm
 
 # Установка базовой системы
-echo "Устанавливаем пакеты..."
+echo "Installing base packages..."
 pacstrap /mnt base linux linux-firmware base-devel networkmanager sudo nano git
 
 # Настройка системы
-echo "Настраиваем систему..."
-genfstab -U /mnt >> /mnt/etc/fstab
+echo "Configuring system..."
+genfstab -U /mnt > /mnt/etc/fstab
 
 arch-chroot /mnt /bin/bash <<EOF
 # Часовой пояс
-ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+ln -sf "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
 hwclock --systohc
 
 # Локализация
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 # Пользователь
-useradd -m -G wheel -s /bin/bash macuser
-echo "macuser:$PASSWORD" | chpasswd
+useradd -m -G wheel -s /bin/bash archuser
+echo "archuser:$PASSWORD" | chpasswd
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 
 # Сеть
@@ -57,6 +64,7 @@ echo "blacklist brcmfmac" > /etc/modprobe.d/wl.conf
 # Загрузчик
 bootctl --path=/boot install
 echo -e "title Arch Linux\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\noptions root=$ROOT_PART rw" > /boot/loader/entries/arch.conf
+
 EOF
 
-echo "Установка завершена! Перезагрузитесь командой: reboot"
+echo "Installation complete! Reboot with: reboot"
