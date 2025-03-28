7db780cd5bad45cda013eca36dda50db46fa09f6
Andrey <alcaponedev@gmail.com>
2025-03-28 17:30:41 +0300

    fix from mac

--- apps.sh
+++ apps.sh
@@ -1,16 +1,18 @@
 sudo pacman -S \
-    ranger \                # файловый менеджер
-    pass \                  # менеджер паролей
-    lm_sensors \            # датчики
-    openssh    \            # ssh client
-    lsd        \            # analog ls
-    git-delta  \            # plugin fo git
-    firefox    \
-    telegram-desktop \
-    mattermost-desktop \
-    obsidian \
-    proxychains-ng \
-    gparted \
-    timeshift \
-    libreoffice-still \
-    xclip \                 # копирование в терминале
+  ranger \    # файловый менеджер
+pass \        # менеджер паролей
+lm_sensors \  # датчики
+openssh \     # ssh client
+lsd \         # analog ls
+git-delta \   # plugin fo git
+firefox \
+  telegram-desktop \
+  mattermost-desktop \
+  obsidian \
+  proxychains-ng \
+  gparted \
+  timeshift \
+  libreoffice-still \
+  xclip \    # копирование в терминале
+highlight \  # подсветка кода
+
